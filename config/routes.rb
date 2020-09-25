7dfb8d8ecae040c43fb8c6a92b474978c3a4549d
munemo <john.chimbani@outlook.com>
2020-09-25 21:52:27 +0200

    Adding Devise tocken auth, creating User model, creating a factory for users and unit test for creating orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
     namespace :v0 do
       resources :pings, only: [:index], constraints: { format: 'json' }
     end
-  end
+  
+   namespace :v1 do
+      resources :products, only: [:index], constraints: { format: 'json' }
+    end
+end
 end
