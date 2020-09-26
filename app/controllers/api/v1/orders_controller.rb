fab514277a4e3d9fbb40a83f27847a5bb3dfc5dd
munemo <john.chimbani@outlook.com>
2020-09-26 21:31:33 +0200

    Modifying orders_controller, order_items factory and orders factory. Creating new factory, model and controller for products.

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -2,10 +2,10 @@ class Api::V1::OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def create
-
-    oder = Order.create(user:current_user)
-    order.order_items.create(product:params["product"])
-    render json: {id: order.id, message: "Item has been added to your order"}
+    order = Order.create(user:current_user)
+    order.order_items.create(product_id:params["product"])
+    
+    render json: {order_id: order.id, message: "Item has been added to your order"}
 
   end
 end
