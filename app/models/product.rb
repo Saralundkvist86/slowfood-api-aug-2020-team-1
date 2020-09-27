147a7d9e0895670a28fc490e89280524746ee34b
sara <saralundkvist86@gmail.com>
2020-09-27 14:34:16 +0200

    accepting incoming changes from John, removing doublet migration of product, removing desciption in expected database table in unit test for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,3 @@
 class Product < ApplicationRecord
   validates_presence_of :name, :price
-<<<<<<< HEAD
 end
-=======
-end
->>>>>>> 284deef8e1a84ff5012d0d65a4d0ef91b663a0de
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,12 +1,6 @@
 FactoryBot.define do
   factory :product do
-<<<<<<< HEAD
-    name { 'MyString' }
-    description { 'MyText' }
-    price { 1.5 }
-=======
     name { "MyString" }
     price { 1 }
->>>>>>> 284deef8e1a84ff5012d0d65a4d0ef91b663a0de
   end
 end
